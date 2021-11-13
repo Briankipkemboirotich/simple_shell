ca6c5a8a60d4d64484ba47f6e54f860bdd0dea62
Brian Rotich <briankemboirotich@gmail.com>
2021-11-13 00:06:31 -0800

    Update

--- _builtin/builtin.h
+++ _builtin/builtin.h
@@ -1,4 +1,4 @@
-ifndef _BUILTIN_H
+#ifndef _BUILTIN_H
 #define _BUILTIN_H
 
 #include "../shell.h"
--- shellvar.h
+++ shellvar.h
@@ -46,7 +46,6 @@ typedef struct sev_s
 	list_t *cmd_q;
 	char *shell_d;
 	char *arg0;
-
-	sev_t;
+	char *sev_t;
 }
 #endif
